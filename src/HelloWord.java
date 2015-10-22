4639022c37c4a0a31653fe7261d46489ae6df768
Eunice Muzime <muzimeunice95@gmail.com>
2015-10-22 13:17:17 +0200

    alterado

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,6 +2,8 @@
 public class HelloWord {
 
 	public static void main (String args[]){
-		System.out.println("Hello World");
+		System.out.println("Hello World eeasdfghj");
+		System.out.println("Hello World eeasdfghj");
+
 	}
 }
